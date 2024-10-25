1ea1e6160f4b68cc8bcc9f3be719a21305b4d6e5
BentlyM <metayerbently@gmail.com>
2024-10-25 12:24:46 -0400

    added dashboard ui

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import Card from './_components/card/Card';
+import styles from './dashboard.module.css';
+import RightBar from './_components/rightbar/RightBar';
+import Transactions from './_components/transactions/Transactions';
+import Chart from './_components/chart/Chart';
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <div className={styles.wrapper}>
+      <div className={styles.main}>
+        <div className={styles.cards}>
+          <Card />
+          <Card />
+          <Card />
+        </div>
+        <Transactions />
+        <Chart />
+      </div>
+      <div className={styles.side}>
+        <RightBar />
+      </div>
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
