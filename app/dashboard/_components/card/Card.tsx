56853f1d054ef663b878e61c64e52e41dbcf3b3b
BentlyM <metayerbently@gmail.com>
2024-10-25 10:50:11 -0400

    added some components

---
+++ app/dashboard/_components/card/Card.tsx
@@ -0,0 +1,9 @@
+import styles from './card.module.css';
+
+import React from 'react';
+
+const Card = () => {
+  return <div>Card</div>;
+};
+
+export default Card;
---
+++ app/dashboard/_components/rightbar/RightBar.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './rightbar.module.css';
+
+const RightBar = () => {
+  return <div>RightBar</div>;
+};
+
+export default RightBar;
